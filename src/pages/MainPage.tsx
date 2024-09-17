c6241d0bd1dfa271fe20e3b40180437092233906
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-17 13:19:49 +0300

    add: layout table

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,64 @@
 import { JSX } from "react";
 
 export const MainPage = (): JSX.Element => {
-  return <>MainPage</>;
+  return (
+    <>
+      <table style={{ border: "1" }}>
+        <thead>
+          <tr>
+            <th>№</th>
+            <th></th>
+            <th>Name</th>
+            <th>VWAP (24HR)</th>
+            <th>Change (24HR)</th>
+            <th>Market Cap</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>BTC</td>
+            <td>Bitcoin</td>
+            <td>22.000</td>
+            <td>2.02$</td>
+            <td>446.4</td>
+            <td>23162.22$</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>BTC</td>
+            <td>Bitcoin</td>
+            <td>22.000</td>
+            <td>2.02$</td>
+            <td>446.4</td>
+            <td>23162.22$</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>BTC</td>
+            <td>Bitcoin</td>
+            <td>22.000</td>
+            <td>2.02$</td>
+            <td>446.4</td>
+            <td>23162.22$</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>BTC</td>
+            <td>Bitcoin</td>
+            <td>22.000</td>
+            <td>2.02$</td>
+            <td>446.4</td>
+            <td>23162.22$</td>
+            <td>+</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
