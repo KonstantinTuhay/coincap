4ee28a4239d1bfe64932b7f18cc76f00d2bbd012
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-17 23:18:29 +0300

    add: components and page for each coin information

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages/MainPage/MainPage";
+// import { MainPage } from "./pages/MainPage/MainPage";
 import { Header } from "./components/Header/Header";
+import { CoinInformation } from "./pages/CoinInformation";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Header />}>
-          {/* <Route path="/" element={<CoinInformation />} /> */}
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<CoinInformation />} />
+          {/* <Route path="/" element={<MainPage />} /> */}
         </Route>
       </Routes>
     </>
---
+++ src/components/NameCurrency/index.tsx
@@ -0,0 +1,14 @@
+import { JSX } from "react";
+
+export const NameCurrency = (): JSX.Element => {
+  return (
+    <div>
+      <p>SymbolCurrency (BTC)</p>
+      <p>name(Bitcoin) / USD</p>
+      <div>
+        <p>График</p>
+      </div>
+      <table>Таблица с информацией</table>
+    </div>
+  );
+};
