0c3a1ebf44d8b15cc70ebc66e670b89a39ce6381
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-17 12:10:01 +0300

    add: route main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
