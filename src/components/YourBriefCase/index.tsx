f5557ac5eab95141c8ac84a4f83ae704dd59b5f8
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-18 15:50:52 +0300

    add: button back on main page

--- src/components/YourBriefCase/index.tsx
+++ src/components/YourBriefCase/index.tsx
@@ -1,9 +1,17 @@
 import { JSX } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const YourBriefcase = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div>
       <h2>Investment Portfolio</h2>
+      <button onClick={() => handleClick()}>Back</button>
       <p>LIST</p>
       <p>Total: number $</p>
     </div>
