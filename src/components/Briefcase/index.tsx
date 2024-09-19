913a644143f1086ce25797e4dc1d3bf111141501
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-19 14:22:04 +0300

    add: feature with show your summary amount

--- src/components/Briefcase/index.tsx
+++ src/components/Briefcase/index.tsx
@@ -1,7 +1,15 @@
 import { JSX } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../hooks/hooks";
 
 export const Briefcase = (): JSX.Element => {
+  const getTotal = useAppSelector((state) => state.listMyCoins);
+  const amounts = getTotal.reduce(
+    (accum, item) => (accum += +item.priceUsd),
+    0
+  );
+  console.log(amounts);
+
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -22,7 +30,7 @@ export const Briefcase = (): JSX.Element => {
         }}
       >
         <p>Total:</p>
-        <p>SUM USD</p>
+        <p>{amounts} USD</p>
       </div>
     </div>
   );
