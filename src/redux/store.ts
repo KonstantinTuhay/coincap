820609746bdd9f6750b04930f5e4e6e18a890011
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-20 15:14:00 +0300

    add: redux-persist and middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,58 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiCoins } from "./apiCoins";
 import popularCoins from "./slices/popularCoins";
 import getDetailsCoin from "./slices/getDetailsCoin";
 import getQuantityCoins from "./slices/getQuantityCoins";
 import listMyCoins from "./slices/listMyCoins";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers(
+  {
     listMyCoins: listMyCoins,
     getQuantityCoins: getQuantityCoins,
     getDetailsCoin: getDetailsCoin,
     popularCoins: popularCoins,
     [apiCoins.reducerPath]: apiCoins.reducer,
+  }
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(apiCoins.middleware),
+);
+
+const persistorReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistorReducer,
+  // {
+  //   listMyCoins: listMyCoins,
+  //   getQuantityCoins: getQuantityCoins,
+  //   getDetailsCoin: getDetailsCoin,
+  //   popularCoins: popularCoins,
+  //   [apiCoins.reducerPath]: apiCoins.reducer,
+  // },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiCoins.middleware);
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiCoins.middleware),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
