a49a4d9ee086c33fe27337831cb029141518d44b
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-24 01:31:51 +0300

    add: styled for user's case

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { Header } from "./components/Header/Header";
 import { CoinInformation } from "./pages/CoinInformation";
-import { YourBriefcase } from "./components/YourBriefCase";
-// import { AddCoin } from "./components/AddCoinModal";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
         <Route path="/" element={<Header />}>
           <Route path="/" element={<MainPage />} />
           <Route path="coininformation" element={<CoinInformation />} />
-          <Route path="yourbriefcase" element={<YourBriefcase />} />
-          {/* <Route path="addcoin" element={<AddCoin />} /> */}
         </Route>
       </Routes>
     </>
--- src/components/Briefcase/index.tsx
+++ src/components/Briefcase/index.tsx
@@ -1,41 +1,52 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 import { useAppSelector } from "../../hooks/hooks";
-import { useNavigate } from "react-router-dom";
 import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { YourBriefcaseModal } from "../YourBriefCaseModal";
 
 export const Briefcase = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
+
   const getTotal = useAppSelector((state) => state.listMyCoins);
   const amounts = getTotal
     .reduce((accum, item) => accum + +item.priceUsd * +item.quantity, 0)
     .toFixed(2);
 
-  const navigate = useNavigate();
-
   const handleClick = () => {
-    navigate("/yourbriefcase");
+    console.log(1111);
+    setOpen(true);
+    console.log(open);
+
+    console.log(1111);
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        marginRight: "300px",
-      }}
-    >
-      <div style={{ cursor: "pointer" }} onClick={() => handleClick()}>
-        <WorkOutlineOutlinedIcon sx={{ fontSize: 80 }} />
-      </div>
+    <>
+      <YourBriefcaseModal open={open} setOpen={setOpen} />
+
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          margin: "15px 0px 15px 10px",
+          marginRight: "300px",
         }}
       >
-        <Typography>Total:</Typography>
-        <Typography>{amounts} USD</Typography>
+        <div style={{ cursor: "pointer" }}>
+          <WorkOutlineOutlinedIcon
+            sx={{ fontSize: 80 }}
+            onClick={() => handleClick()}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "15px 0px 15px 10px",
+          }}
+        >
+          <Typography>Total:</Typography>
+          <Typography>{amounts} USD</Typography>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
