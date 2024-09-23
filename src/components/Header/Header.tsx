7773c04bf4e6abca14e3119003e6ceadadd17216
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-24 00:03:44 +0300

    add: styled for popular coins

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,20 @@ import { PopularСryptoCurrencies } from "../PopularСryptoCurrencies";
 import { Briefcase } from "../Briefcase";
 import { Outlet } from "react-router-dom";
 import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
 
 export const Header = (): JSX.Element => {
   return (
     <>
       <Container maxWidth="lg">
-        <div
+        <Box
           style={{
             display: "flex",
           }}
         >
           <PopularСryptoCurrencies />
           <Briefcase />
-        </div>
+        </Box>
 
         <Outlet />
       </Container>
