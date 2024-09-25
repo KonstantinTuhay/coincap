11daef98098ef9d7af64d78dd505171470cc201b
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-25 16:56:42 +0300

    change: style for popular coin

--- src/components/PopularСryptoCurrencies/index.tsx
+++ src/components/PopularСryptoCurrencies/index.tsx
@@ -2,22 +2,30 @@ import { JSX } from "react";
 import { useAppSelector } from "../../hooks/hooks";
 import { PopularCoin } from "../PopularCoin";
 import { Box, Typography } from "@mui/material";
+import styles from "../../styles/PopularСryptoCurrencies/index.module.css";
 
 export const PopularСryptoCurrencies = (): JSX.Element => {
   const popularCoins = useAppSelector((state) => state.popularCoins);
 
   return (
     <Box
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        marginLeft: "170px",
-      }}
+      className={styles.box}
+      // style={{
+      //   display: "flex",
+      //   flexDirection: "column",
+      //   marginLeft: "170px",
+      // }}
     >
-      <Typography sx={{ textDecoration: "underline", ml: "20px" }}>
+      <Typography
+        className={styles.popular}
+        // sx={{ textDecoration: "underline", ml: "20px" }}
+      >
         Popular cryptocurrencies:
       </Typography>
-      <Box style={{ display: "flex", flexDirection: "row", marginTop: "5px" }}>
+      <Box
+        className={styles.boxCoin}
+        // style={{ display: "flex", flexDirection: "row", marginTop: "5px" }}
+      >
         {popularCoins.map((popularCoin) => (
           <PopularCoin key={crypto.randomUUID()} popularCoin={popularCoin} />
         ))}
