260e045368ac96d0c76fbfd9e5a933584f6d015e
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-25 17:10:34 +0300

    change: style for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,18 @@ import { PopularСryptoCurrencies } from "../PopularСryptoCurrencies";
 import { Briefcase } from "../Briefcase";
 import { Outlet } from "react-router-dom";
 import { Box, Container } from "@mui/material";
+import styles from "../../styles/Header/index.module.css";
 
 export const Header = (): JSX.Element => {
   return (
     <Container maxWidth="lg">
       <Box
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: "15px",
-        }}
+        className={styles.box}
+        // style={{
+        //   display: "flex",
+        //   justifyContent: "space-between",
+        //   marginTop: "15px",
+        // }}
       >
         <PopularСryptoCurrencies />
         <Briefcase />
